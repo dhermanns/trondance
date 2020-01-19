a87a16bff712be7de5ee1b1f9eff6dbb660bb066
dirk <h3rmanns@gmail.com>
2020-01-19 22:32:40 +0100

    Added Playback of mp3 files

--- src/main/java/trondance/Controller.java
+++ src/main/java/trondance/Controller.java
@@ -4,21 +4,45 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 
 import javax.inject.Named;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 
 @Named
 public class Controller {
 
+    private Media media = new Media(new File("trondance.mp3").toURI().toString());
+    private MediaPlayer mediaPlayer = new MediaPlayer(media);
+    private SimpleDateFormat playTimeFormatter = new SimpleDateFormat("mm:ss:SSS");
+
     @FXML
     TextField nodeMcu1;
+    @FXML
+    Label mediaPlayerCurrentTimeLabel;
 
     @Autowired
     RestTemplate restTemplate;
+
+    public Controller() {
+        mediaPlayer.currentTimeProperty().addListener((observable, oldTime, newTime) -> {
+
+            String formattedTime = playTimeFormatter.format(mediaPlayer.getCurrentTime().toMillis());
+            mediaPlayerCurrentTimeLabel.setText(formattedTime);
+        });
+    }
+
+    @FXML
+    private void handlePlay(ActionEvent event) {
+        mediaPlayer.play();
+    }
 
     @FXML
     private void handleFirstFlash(ActionEvent event) {
